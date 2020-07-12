6cadbfe60b9823494714f69d99776b0d17a58abb
Alphamplyer <even@pouyadou.fr>
2020-07-12 17:27:11 +0200

    Test another way to do component (by class)

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 
-function Welcome ({name}) {
+function WelcomeFunc ({name}) {
     return <h1>Bonjour {name}</h1>
+}
+
+class Welcome extends React.Component {
+
+    constructor (props) {
+        super();
+        this.name = props.name;
+    }
+
+    render() {
+        return <h1>Bonjour {this.name}</h1>
+    }
 }
 
 ReactDOM.render(<Welcome name="Jean"/>, document.querySelector('#app'));
